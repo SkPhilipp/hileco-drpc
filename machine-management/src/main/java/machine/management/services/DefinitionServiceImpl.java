4e66d85828183e63a77bb68c3f565b0be9dd7ad6
Philipp Gayret <pgayret@gmail.com>
2014-05-01 00:06:23 +0200

    Cosmetic fixes

--- machine-management/src/main/java/machine/management/services/DefinitionServiceImpl.java
+++ machine-management/src/main/java/machine/management/services/DefinitionServiceImpl.java
@@ -2,7 +2,6 @@ package machine.management.services;
 
 import machine.management.model.Definition;
 import machine.management.services.generic.AbstractModelService;
-import machine.management.services.DefinitionService;
 
 public class DefinitionServiceImpl extends AbstractModelService<Definition> implements DefinitionService {
 
