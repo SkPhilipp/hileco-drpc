40d94f556312ac5550b2fa0623ec30515195109a
Philipp Gayret <pgayret@gmail.com>
2014-05-05 23:08:04 +0200

    Its beautiful

--- machine-backbone/src/test/java/CallManagement.java
+++ machine-backbone/src/test/java/CallManagement.java
@@ -1,17 +1,28 @@
 import machine.lib.client.messaging.NetworkMessage;
-import machine.lib.client.messaging.NetworkMessageRouter;
-import org.junit.Ignore;
+import machine.management.api.services.MessageService;
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-@Ignore
 public class CallManagement {
+
+    public static final String MANAGEMENT_URL = "http://localhost:8080/services/";
+    private static MessageService messageService;
+
+    @BeforeClass
+    public static void beforeClass() {
+        ResteasyClient client = new ResteasyClientBuilder().build();
+        ResteasyWebTarget target = client.target(MANAGEMENT_URL);
+        messageService = target.proxy(MessageService.class);
+    }
 
     @Test
     public void go() throws Exception {
         for (int i = 0; i < 10; i++) {
             NetworkMessage<String> stringNetworkMessage = new NetworkMessage<>("hello", "hello world");
-            NetworkMessageRouter networkMessageRouter = new NetworkMessageRouter();
-            networkMessageRouter.submit("http://localhost:8080/services/messages/publish", stringNetworkMessage);
+            messageService.publish(stringNetworkMessage);
         }
     }
 
